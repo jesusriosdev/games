9922719e9d9896d74becc08b946dff33432b5e18
Jesus Rios <etxcoding@gmail.com>
2020-11-14 10:25:46 -0800

    tic tac toe semicomplete

--- tic-tac-toe/js/script.js
+++ tic-tac-toe/js/script.js
@@ -1,74 +1,114 @@
+let winner = '';
+let numberOfTurns = 0;
+let turn = 1;
+
+const values = ['X', 'O'];
+let moves = [];
+
 document.addEventListener('DOMContentLoaded', (e) => {
-
-    init();
-
-    document.querySelectorAll('.square').forEach((square) => {
-        square.addEventListener('click', (ee) => {
-            // console.log(square);
-            square.innerHTML = values[turn - 1];
-            let winner = checkForWinner();
-            console.log(winner);
-            if(winner) {
-                alert(`Winner: Player ${winner == 'X' ? 1 : 2}!`);
-            }
-
-            turn = turn == 1 ? 2 : 1;
-        });
-    });
-
 });
 
-let turn = 1;
-let winner = null;
-
-let xValue = 'X';
-let oValue = 'O';
-
-let values = ['X', 'O'];
+init();
 
 function init() {
     console.log('Starting new game, have fun!');
-
+    
+    winner = '';
+	numberOfTurns = 0;
     turn = 1;
-    document.querySelectorAll('.square').forEach((square) => {
-        square.innerHTML = '';
+    moves = [];
+
+    const playerOne = document.querySelector(`.player-one`);
+    const playerTwo = document.querySelector(`.player-two`);
+    playerOne.classList.remove('active');
+    playerTwo.classList.remove('active');
+    playerOne.classList.add('active');
+
+	document.querySelectorAll('.square').forEach((square) => {
+		square.innerHTML = '';
     });
+
+    const resultsContainer = document.querySelector(`.results-container`);
+    resultsContainer.innerHTML = ``;
+}
+
+function move(number) {
+    console.log(`Number clicked: ${number}`);
+    // console.log(moves.includes(number));
+    // console.log(moves.includes(number) === false && winner === '');
+    if(moves.includes(number) === false && winner === '') {
+
+        moves.push(number);
+        numberOfTurns++;
+
+        const square = document.querySelector(`div[data-key="${number}"]`);
+        square.innerHTML = values[turn - 1];
+        
+        winner = checkForWinner();
+        if (winner) {
+            const resultsContainer = document.querySelector(`.results-container`);
+            resultsContainer.innerHTML = `<h1>Winner: Player ${turn}!</h1>`;
+            return;
+        }
+        
+        if(numberOfTurns === 9) {
+            winner = '/';
+
+            const resultsContainer = document.querySelector(`.results-container`);
+            resultsContainer.innerHTML = `<h1>Winner: THIS IS A TIE!</h1>`;
+            return;
+        }
+
+        const playerOne = document.querySelector(`.player-one`);
+        const playerTwo = document.querySelector(`.player-two`);
+        if(turn === 1) {
+
+            turn = 2;
+            playerOne.classList.remove('active');
+            playerTwo.classList.add('active');
+        } else {
+
+            turn = 1;
+            playerTwo.classList.remove('active');
+            playerOne.classList.add('active');
+        }
+    }
 }
 
 function checkForWinner() {
-    let square1 = document.getElementById('square1');
-    let square2 = document.getElementById('square2');
-    let square3 = document.getElementById('square3');
-    let square4 = document.getElementById('square4');
-    let square5 = document.getElementById('square5');
-    let square6 = document.getElementById('square6');
-    let square7 = document.getElementById('square7');
-    let square8 = document.getElementById('square8');
-    let square9 = document.getElementById('square9');
-    
+	let square1 = document.querySelector(`div[data-key="${1}"]`);
+	let square2 = document.querySelector(`div[data-key="${2}"]`);
+	let square3 = document.querySelector(`div[data-key="${3}"]`);
+	let square4 = document.querySelector(`div[data-key="${4}"]`);
+	let square5 = document.querySelector(`div[data-key="${5}"]`);
+	let square6 = document.querySelector(`div[data-key="${6}"]`);
+	let square7 = document.querySelector(`div[data-key="${7}"]`);
+	let square8 = document.querySelector(`div[data-key="${8}"]`);
+	let square9 = document.querySelector(`div[data-key="${9}"]`);
 
-    values.forEach((v) => {
+	for (const v of values) {
+		if (checkLines(square1, square2, square3, v)) return v;
+		if (checkLines(square1, square5, square9, v)) return v;
+		if (checkLines(square1, square4, square7, v)) return v;
 
-        // Posible winner moves.
-        if(checkLines(square1, square2, square3, v)) return v;
-        if(checkLines(square1, square5, square9, v)) return v;
-        if(checkLines(square1, square4, square7, v)) return v;
-    
-        if(checkLines(square2, square5, square8, v)) return v;
-    
-        if(checkLines(square3, square6, square9, v)) return v;
-    
-        if(checkLines(square4, square5, square6, v)) return v;
-    
-        if(checkLines(square7, square8, square9, v)) return v;
-    });
+		if (checkLines(square2, square5, square8, v)) return v;
 
-    return '';
+		if (checkLines(square3, square6, square9, v)) return v;
+		if (checkLines(square3, square5, square7, v)) return v;
+
+		if (checkLines(square4, square5, square6, v)) return v;
+
+		if (checkLines(square7, square8, square9, v)) return v;
+	}
+
+	return '';
 }
 
 function checkLines(square1, square2, square3, value) {
-    console.log(square1);
-    console.log(square2);
-    console.log(square3);
-    if(square1.innerHTML == value && square2.innerHTML == value && square3.innerHTML == value) return true;
+	if (
+		square1.innerHTML == value &&
+		square2.innerHTML == value &&
+		square3.innerHTML == value
+	)
+		return true;
 }
